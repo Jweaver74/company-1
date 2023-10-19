1c0efdb98f68ce56515eb3260bdee8003810c53f
Jason Weaver <119889739+Jweaver74@users.noreply.github.com>
2023-10-19 12:16:53 -0500

    added background color

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 
+import React from 'react';
 import './App.css';
+import Hero from './Hero';
 
 function App() {
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hetherington Honey</h1>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav className="navbar">
+      <Hero />
+        <ul>
+          <li><a href="#about-us">About Us</a></li>
+          <li><a href="#bees-life">A Bee's Life</a></li>
+          <li><a href="#contact-us">Contact Us</a></li>
+        </ul>
+      </nav>
+
+    
+      <section id="about-us">
+        {/* Your About Us content goes here */}
+      </section>
+      <section id="bees-life">
+        {/* Your A Bee's Life content goes here */}
+      </section>
+      <section id="contact-us">
+        {/* Your Contact Us content goes here */}
+      </section>
     </div>
   );
 }
 
 export default App;
+
---
+++ src/Hero.js
@@ -0,0 +1,14 @@
+// Hero.js
+import React from 'react';
+import './Hero.css';
+
+const Hero = () => {
+  return (
+    <div className="hero">
+      <h1>Welcome to Hetherington Honey</h1>
+      <p>Your Source for Premium Honey Products</p>
+    </div>
+  );
+}
+
+export default Hero;
