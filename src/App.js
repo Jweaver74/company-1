cc354cc229ca88dde9a8f65658be74b5484a339d
Jason Weaver <119889739+Jweaver74@users.noreply.github.com>
2023-10-19 15:37:50 -0500

    updated contact us

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Hero from './components/hero/Hero';
 import About from './components/about/About';
 import Bees from './components/bees life/Bees';
+import Contact from './components/contact/ContactUs';
 
 function App() {
   
@@ -12,9 +13,9 @@ function App() {
       <nav className="navbar">
       <Hero />
         <ul class="navBar-links">
-          <li><a href="#about">About Us</a></li>
-          <li><a href="#bees-life">A Bee's Life</a></li>
-          <li><a href="#contact-us">Contact Us</a></li>
+          <li><a href="#about"class="nav-link">About Us</a></li>
+          <li><a href="#bees-life"class="nav-link">A Bee's Life</a></li>
+          <li><a href="#contact-us"class="nav-link">Contact Us</a></li>
         </ul>
       </nav>
 
@@ -27,9 +28,13 @@ function App() {
         <Bees/>
         {/* Your A Bee's Life content goes here */}
       </section>
-      <section id="contact-us">
+
+
+
+      <footer id="contact-us">
+        <Contact/>
         {/* Your Contact Us content goes here */}
-      </section>
+      </footer>
     </div>
   );
 }
---
+++ src/components/contact/ContactUs.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ContactUs = () => {
+  return (
+    <div className="contact-us">
+      <h2>Contact Us</h2>
+      <p>Email: <a href="mailto:jamaweaver@yahoo.com" style={{color:"white",cursor: "pointer"}}>jamaweaver@yahoo.com</a></p>
+      <p>Phone: (817)371-6046</p>
+    </div>
+  );
+}
+
+export default ContactUs;
