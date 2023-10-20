30e4bae7d10950e394e17bc800e6004250e29361
Jason Weaver <119889739+Jweaver74@users.noreply.github.com>
2023-10-20 12:43:10 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,43 +1,60 @@
-
-import React from 'react';
-import './App.css';
-import Hero from './components/hero/Hero';
-import About from './components/about/About';
-import Bees from './components/bees life/Bees';
-import Contact from './components/contact/ContactUs';
+import React from "react";
+import "./App.css";
+import Hero from "./components/hero/Hero";
+import About from "./components/about/About";
+import Bees from "./components/bees life/Bees";
+import Contact from "./components/contact/ContactUs";
+import HomeIcon from "./components/icons/home.png"
 
 function App() {
-  
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  }
   return (
     <div className="App">
       <nav className="navbar">
-      <Hero />
+        <Hero />
         <ul class="navBar-links">
-          <li><a href="#about"class="nav-link">About Us</a></li>
-          <li><a href="#bees-life"class="nav-link">A Bee's Life</a></li>
-          <li><a href="#contact-us"class="nav-link">Contact Us</a></li>
+          <li>
+            <a href="#about" class="nav-link">
+              About Us
+            </a>
+          </li>
+          <li>
+            <a href="#bees-life" class="nav-link">
+              A Bee's Life
+            </a>
+          </li>
+          <li>
+            <a href="#contact-us" class="nav-link">
+              Contact Us
+            </a>
+          </li>
         </ul>
       </nav>
 
-    
       <section id="about">
-        <About/>
+        <About />
         {/* Your About Us content goes here */}
       </section>
       <section id="bees-life">
-        <Bees/>
+        <Bees />
         {/* Your A Bee's Life content goes here */}
       </section>
 
-
-
       <footer id="contact-us">
-        <Contact/>
+        <Contact />
         {/* Your Contact Us content goes here */}
       </footer>
+
+      <button className="home-icon" onClick={scrollToTop}>
+        <img src={HomeIcon} alt="home icon" />
+      </button>
     </div>
   );
 }
 
 export default App;
-
