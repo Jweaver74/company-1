5017cc45ed01b581ce65c1cc263e988b4eac44c9
Jason Weaver <119889739+Jweaver74@users.noreply.github.com>
2023-10-20 14:00:09 -0500

    added bee photo

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -5,6 +5,7 @@ import './Hero.css';
 const Hero = () => {
   return (
     <div className="hero">
+     <img src={process.env.PUBLIC_URL + '/images/beehive.jpg'} alt="Beehive" style={{maxWidth: '200px', height: 'auto'}}/>
       <h1>Welcome to Hetherington Honey</h1>
       <p>Your Source for Premium Honey</p>
     </div>
