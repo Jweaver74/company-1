4d9a7455f7323867879be937ade01e758f0fa488
Jason Weaver <119889739+Jweaver74@users.noreply.github.com>
2023-10-25 09:53:47 -0500

    added pictures and video

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -6,10 +6,23 @@ const About = () => {
   return (
     <div className="about">
       <h2>About Us</h2>
-      <p>We take great pride in the art of honey harvesting.
-        Our beekeepers follow a careful and sustainable process to 
-        ensure the highest quality honey.
+      <div className="video-container">
+        <a href={process.env.PUBLIC_URL + '/images/beevideo.mp4'} target="_blank" rel="noreferrer">
+        <div className="video-wrapper">
+            <video className="video" controls>
+              <source src={process.env.PUBLIC_URL + '/images/beevideo.mp4'} type="video/mp4" />
+              Your browser does not support the video tag.
+            </video>
+          <img src={process.env.PUBLIC_URL + '/images/beeframe.jpg'} alt="video Thumbnail" width="400" />
+          </div>
+          <p>Click here to watch the bees in action</p>
+        </a>
+      </div>
+      <p>Hetherington honey started when we picked up our first hive. We intended on it to just bee
+        a fun thing to do while supporting the pollinator population. 
       </p>
+     
+     
     </div>
   );
 }
--- src/components/honey/Honey.js
+++ src/components/honey/Honey.js
@@ -5,7 +5,24 @@ const Honey = () => {
     return (
         <div className="honey">
             <h2>Our Honey</h2>
-            <p>We take great pride in the art of honey harvesting. Our beekeepers follow a careful and sustainable process to ensure the highest quality honey.</p>
+            <div className="honey">
+                <img src = {process.env.PUBLIC_URL + '/images/honey.jpg'} alt="Honey" style={{maxWidth: '400px', height: '200px', float:"right"}}/>
+            <p>We take great pride in the art of honey harvesting.Our first year we had ours bees, 
+        we did weakly hive inspections. Upon one inspection, one of out frames fell apart and we had to harvest the honey
+        from that frame. We were amazed at the taste of the honey. We replaced the broken frame with a new one in hopes they would make
+        more for the following year. Our bees made it through out mild Texas winter and were all a buzz
+        the next spring with everything blooming. In spring we did another inspection and found that they had filled up every frame in the 
+        hive so we needed to help them out. We remove 3 frames and replaced with new ones. 
+        We then harvest the honey from the three frames. The honey from those frames was so tasty that we handed out small bottle to all of the 
+        neighbors. </p>
+        <p> Come that summer, Texas had a drought. We left the hive alone and made sure there was plenty of water near by if they need it.
+        We did a hive inspection near the end of the drought to make sure things were still going good. The bees had filled all the frames again, so we replaced three full ones with three new ones. 
+        The honey from the three harvested frames was a darker than normal color. Tasting it, you could taste hints of
+        pecan and mesquite. After doing some research we found that during a drought bees will actully harvest the sap 
+        from pecan trees giving the honey a unique taste know as honeydew honey.
+
+      </p>
+        </div>
         </div>
     );
 }
